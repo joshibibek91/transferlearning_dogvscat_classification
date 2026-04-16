8bf10d509a62b7d1d388d653659bcac48d9f40fa
Bibek <bibek.joshi91@gmail.com>
2026-04-16 11:10:16 +0545

    d

--- app.py
+++ app.py
@@ -14,14 +14,13 @@ import numpy as np
 from tensorflow import keras
 from keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.models import load_model
 
 from PIL import Image
 # import matplotlib.pyplot as plt
 # import matplotlib.image as img
 
 # creating a object
-model = load_model('my_model.keras', compile=False)
+model = keras.models.load_model('my_model.keras')
 path = st.file_uploader("Upload an image of a cat or a dog", type=["jpg", "jpeg", "png"])
 
 
